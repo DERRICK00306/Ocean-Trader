48c9b7bc6a4c783db04da5fc86a08d94cef46552
SongZiyan <u3563700@connect.hku.hk>
2023-03-17 20:44:45 +0800

    3.17 Asia Push

--- voyage/voyage/Assets/scripts/sail.cs
+++ voyage/voyage/Assets/scripts/sail.cs
@@ -6,6 +6,8 @@ public class sail : MonoBehaviour
 {
     public GameObject[] HKSingapore;
     public GameObject[] HKTokyo;
+    public GameObject[] SingaporeTokyo;
+    public GameObject[] HKSydney;
     string route;
     int i = 0;
     // Start is called before the first frame update
@@ -28,6 +30,11 @@ public class sail : MonoBehaviour
             {
                 route = "0-1";
             }
+
+            if (transform.position == SingaporeTokyo[3].transform.position)
+            {
+                route = "2-1";
+            }
         }
 
 
@@ -45,6 +52,11 @@ public class sail : MonoBehaviour
             {
                 route = "2-0";
             }
+
+            if (transform.position == HKSydney[4].transform.position)
+            {
+                route = "3-0";
+            }
         }
 
 
@@ -55,6 +67,22 @@ public class sail : MonoBehaviour
             if (transform.position == HKTokyo[0].transform.position)
             {
                 route = "0-2";
+            }
+
+            if (transform.position == SingaporeTokyo[0].transform.position)
+            {
+                route = "1-2";
+            }
+        }
+
+
+
+        if (Input.GetMouseButton(0) && hit.collider != null && hit.collider.gameObject.name == "Sydney")
+        {
+
+            if (transform.position == HKSydney[0].transform.position)
+            {
+                route = "0-3";
             }
         }
 
@@ -182,5 +210,156 @@ public class sail : MonoBehaviour
             }
         }
 
+
+
+
+        if (route == "0-3")
+        {
+            if (i == 0 && transform.position != HKSydney[1].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[1].transform.position, 0.01f);
+            }
+            if (transform.position == HKSydney[1].transform.position)
+            {
+                i = 1;
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[2].transform.position, 0.01f);
+            }
+            if (i == 1 && transform.position != HKSydney[2].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[2].transform.position, 0.01f);
+            }
+            if (transform.position == HKSydney[2].transform.position)
+            {
+                i = 2;
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[3].transform.position, 0.01f);
+            }
+            if (i == 2 && transform.position != HKSydney[3].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[3].transform.position, 0.01f);
+            }
+            if (transform.position == HKSydney[3].transform.position)
+            {
+                i = 3;
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[4].transform.position, 0.01f);
+            }
+            if (i == 3 && transform.position != HKSydney[4].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[4].transform.position, 0.01f);
+            }
+            if (transform.position == HKSydney[4].transform.position)
+            {
+                i = 0;
+                route = "";
+            }
+        }
+
+
+
+        if (route == "3-0")
+        {
+            if (i == 0 && transform.position != HKSydney[3].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[3].transform.position, 0.01f);
+            }
+            if (transform.position == HKSydney[3].transform.position)
+            {
+                i = 1;
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[2].transform.position, 0.01f);
+            }
+            if (i == 1 && transform.position != HKSydney[2].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[2].transform.position, 0.01f);
+            }
+            if (transform.position == HKSydney[2].transform.position)
+            {
+                i = 2;
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[1].transform.position, 0.01f);
+            }
+            if (i == 2 && transform.position != HKSydney[1].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[1].transform.position, 0.01f);
+            }
+            if (transform.position == HKSydney[1].transform.position)
+            {
+                i = 3;
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[0].transform.position, 0.01f);
+            }
+            if (i == 3 && transform.position != HKSydney[0].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, HKSydney[0].transform.position, 0.01f);
+            }
+            if (transform.position == HKSydney[0].transform.position)
+            {
+                i = 0;
+                route = "";
+            }
+        }
+
+
+
+
+        if (route == "1-2")
+        {
+            if (i == 0 && transform.position != SingaporeTokyo[1].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[1].transform.position, 0.01f);
+            }
+            if (transform.position == SingaporeTokyo[1].transform.position)
+            {
+                i = 1;
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[2].transform.position, 0.01f);
+            }
+            if (i == 1 && transform.position != SingaporeTokyo[2].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[2].transform.position, 0.01f);
+            }
+            if (transform.position == SingaporeTokyo[2].transform.position)
+            {
+                i = 2;
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[3].transform.position, 0.01f);
+            }
+            if (i == 2 && transform.position != SingaporeTokyo[3].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[3].transform.position, 0.01f);
+            }
+            if (transform.position == SingaporeTokyo[3].transform.position)
+            {
+                i = 0;
+                route = "";
+            }
+        }
+
+
+
+        if (route == "2-1")
+        {
+            if (i == 0 && transform.position != SingaporeTokyo[2].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[2].transform.position, 0.01f);
+            }
+            if (transform.position == SingaporeTokyo[2].transform.position)
+            {
+                i = 1;
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[1].transform.position, 0.01f);
+            }
+            if (i == 1 && transform.position != SingaporeTokyo[1].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[1].transform.position, 0.01f);
+            }
+            if (transform.position == SingaporeTokyo[1].transform.position)
+            {
+                i = 2;
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[0].transform.position, 0.01f);
+            }
+            if (i == 2 && transform.position != SingaporeTokyo[0].transform.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, SingaporeTokyo[0].transform.position, 0.01f);
+            }
+            if (transform.position == SingaporeTokyo[0].transform.position)
+            {
+                i = 0;
+                route = "";
+            }
+        }
     }
 }

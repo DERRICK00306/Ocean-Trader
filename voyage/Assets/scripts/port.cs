44a21c44dd5ca144fff7dbb4f8d7c0a1f2e7c5aa
Derrick <derrick00306@gmail.com>
2023-03-19 18:15:10 +0800

    3.19 Commit
    Create sample level 2(Japan)
    Add change scene and sound effect
    Build Menu Scene

--- voyage/Assets/scripts/port.cs
+++ voyage/Assets/scripts/port.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class port : MonoBehaviour
 {
-    public float sleep_time = 2;
+    public float sleep_time = 4;
     public GameObject cloud1, cloud2;
+    public AudioSource audio_source;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +17,13 @@ public class port : MonoBehaviour
     IEnumerator OnTriggerEnter2D(Collider2D obj)
     {
         string name = obj.gameObject.name;
-        yield return new WaitForSeconds(sleep_time);
+        
         Instantiate(cloud1, new Vector2(-18,-6),Quaternion.identity);
         Instantiate(cloud2, new Vector2(18,8),Quaternion.identity);
+        Instantiate(audio_source);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Japan");
+
     }
         // Update is called once per frame
         void Update()

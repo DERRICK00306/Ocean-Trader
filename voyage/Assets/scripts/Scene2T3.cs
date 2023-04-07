c761ee28c81f09bb82fdf17132b935db593c1581
SongZiyan <u3563700@connect.hku.hk>
2023-04-07 16:54:02 +0800

    4.7 final

--- voyage/Assets/scripts/Scene2T3.cs
+++ voyage/Assets/scripts/Scene2T3.cs
@@ -6,17 +6,10 @@ using UnityEngine.SceneManagement;
 public class Scene2T3 : MonoBehaviour
 {
     public string Scene_Name;
-    public GameObject cloud1, cloud2;
-    public AudioSource audio_source;
     // Start is called before the first frame update
     public void change_scene()
     {
-        Instantiate(cloud1, new Vector2(-18, -6), Quaternion.identity);
-        Instantiate(cloud2, new Vector2(18, 8), Quaternion.identity);
-        Instantiate(audio_source);
-        Invoke("change", 3);
+        SceneManager.LoadScene(Scene_Name);
     }
-
-    public void change() { SceneManager.LoadScene(Scene_Name); }
-
+    
 }

4bdcf5dd97120a6be52468cc7ba6046d226ce999
Larry-grd <65031200+Larry-grd@users.noreply.github.com>
2023-04-17 17:02:24 +0800

    4.17

--- voyage/Assets/scripts/map/timemoney.cs
+++ voyage/Assets/scripts/map/timemoney.cs
@@ -6,17 +6,21 @@ using TMPro;
 public class timemoney : MonoBehaviour
 {
     public TextMeshProUGUI Text1;
+    public TextMeshProUGUI Text2;
+    public TextMeshProUGUI Text3;
     // Start is called before the first frame update
-    void Start()
+    /*void Start()
     {
         Text1 = transform.GetComponent<TextMeshProUGUI>();
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
         int moneyoutput = Global.Money;
         int carryoutput = Global.Carry;
-        Text1.text = "Money: " + moneyoutput + "            Carry: "+carryoutput + "/100            Time: " + timer.countyear + "/" + timer.countmonth;
+        Text1.text = "" + moneyoutput;
+        Text2.text = "" + carryoutput + "/" + Global.MaxLoad;
+        Text3.text = timer.countyear + " / " + timer.countmonth;
     }
 }

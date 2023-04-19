d6bdcf5dcdae0463cfc07169a008a83cc49a8272
Larry-grd <65031200+Larry-grd@users.noreply.github.com>
2023-04-19 11:38:46 +0800

    4.19

--- voyage/Assets/scripts/Scene2T3.cs
+++ voyage/Assets/scripts/Scene2T3.cs
@@ -9,6 +9,24 @@ public class Scene2T3 : MonoBehaviour
     public GameObject cloud1, cloud2;
     public AudioSource audio_source;
     // Start is called before the first frame update
+
+
+    public void CheckChangeScene()
+    {
+        if (Global.MaxLoad > 0)
+        {
+            FindObjectOfType<Camera>().GetComponent<PlaySound>().PlayThisSoundEffect1();
+            Instantiate(cloud1, new Vector2(-18, -6), Quaternion.identity);
+            Instantiate(cloud2, new Vector2(18, 8), Quaternion.identity);
+            Instantiate(audio_source);
+            Invoke("change", 1);
+        }
+        else FindObjectOfType<Camera>().GetComponent<PlaySound>().PlayThisSoundEffect2();
+
+    }
+
+
+
     public void change_scene()
     {
         Instantiate(cloud1, new Vector2(-18, -6), Quaternion.identity);
